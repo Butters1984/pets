bb814046ebd3178abd5dfb47a6d54e7743643870
Rusty <107761493+RustyMods@users.noreply.github.com>
2024-12-14 21:07:36 -0500

    pet manager created

---
+++ Behaviors/SE_Pet.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using BepInEx.Configuration;
+using UnityEngine;
+
+namespace MountPets.Behaviors;
+
+public class SE_Pet : SE_Stats
+{
+    public static ItemDrop.ItemData? m_currentCompanionItem;
+    public static ItemDrop.ItemData? m_tempCompanionItem;
+    public readonly Config config = new();
+    public class Config
+    {
+        public ConfigEntry<float> ItemQualityMultiplier = null!;
+        public ConfigEntry<float> HealthRegen = null!;
+        public ConfigEntry<float> StaminaRegen = null!;
+        public ConfigEntry<float> EitrRegen = null!;
+        public ConfigEntry<float> CarryWeight = null!;
+        public ConfigEntry<float> Speed = null!;
+        public ConfigEntry<float> Jump = null!;
+        public ConfigEntry<float> MaxFallSpeed = null!;
+    }
+
+    public void SetConfigData() => SetConfigData(GetQualityModifier());
+    
+    public void SetConfigData(float qualityModifier)
+    {
+        m_healthRegenMultiplier = config.HealthRegen.Value + qualityModifier * (config.HealthRegen.Value - 1f);
+        m_staminaRegenMultiplier = config.StaminaRegen.Value + qualityModifier * (config.StaminaRegen.Value - 1f);
+        m_eitrRegenMultiplier = config.EitrRegen.Value + qualityModifier * (config.EitrRegen.Value - 1f);
+        m_addMaxCarryWeight = config.CarryWeight.Value * qualityModifier;
+        m_speedModifier = config.Speed.Value * qualityModifier;
+        m_jumpModifier = new Vector3(0f, config.Jump.Value * qualityModifier, 0f);
+        m_maxMaxFallSpeed = config.MaxFallSpeed.Value / qualityModifier;
+    }
+
+    private float GetTempQualityModifier() => m_tempCompanionItem == null ? 1f : config.ItemQualityMultiplier.Value * (m_tempCompanionItem.m_quality - 1) + 1f;
+    
+    private float GetQualityModifier() => m_currentCompanionItem == null ? 1f : config.ItemQualityMultiplier.Value * (m_currentCompanionItem.m_quality - 1) + 1f;
+
+    public override string GetTooltipString()
+    {
+        SetConfigData(GetTempQualityModifier());
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.Append(base.GetTooltipString());
+        return stringBuilder.ToString();
+    }
+}
--- Plugin.cs
+++ Plugin.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Linq;
 using System.Reflection;
 using BepInEx;
 using BepInEx.Configuration;
@@ -6,15 +8,11 @@ using BepInEx.Logging;
 using CreatureManager;
 using HarmonyLib;
 using ItemManager;
-using LocationManager;
-using PieceManager;
+using JetBrains.Annotations;
+using MountPets.Managers;
 using ServerSync;
-using SkillManager;
-using StatusEffectManager;
 using UnityEngine;
-using PrefabManager = ItemManager.PrefabManager;
-using Range = LocationManager.Range;
-
+using CraftingTable = ItemManager.CraftingTable;
 namespace pets
 {
     [BepInPlugin(ModGUID, ModName, ModVersion)]
@@ -24,60 +22,58 @@ namespace pets
         internal const string ModVersion = "1.0.0";
         internal const string Author = "Yggdrah";
         private const string ModGUID = Author + "." + ModName;
-        private static string ConfigFileName = ModGUID + ".cfg";
-        private static string ConfigFileFullPath = Paths.ConfigPath + Path.DirectorySeparatorChar + ConfigFileName;
+        private static readonly string ConfigFileName = ModGUID + ".cfg";
+        private static readonly string ConfigFileFullPath = Paths.ConfigPath + Path.DirectorySeparatorChar + ConfigFileName;
         internal static string ConnectionError = "";
         private readonly Harmony _harmony = new(ModGUID);
-
-        public static readonly ManualLogSource petsLogger =
-            BepInEx.Logging.Logger.CreateLogSource(ModName);
-
-        private static readonly ConfigSync ConfigSync = new(ModGUID)
-            { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion };
+        public static readonly ManualLogSource petsLogger = BepInEx.Logging.Logger.CreateLogSource(ModName);
+        private static readonly ConfigSync ConfigSync = new(ModGUID) { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion };
+        public static petsPlugin Plugin = null!;
         
-        // Location Manager variables
-        public Texture2D tex;
-        private Sprite mySprite;
-        private SpriteRenderer sr;
+        public enum Toggle {On, Off}
+        
+        private static ConfigEntry<Toggle> _serverConfigLocked = null!;
+
+        private void InitConfigs()
+        {
+            _serverConfigLocked = config("General", "Force Server Config", Toggle.On, "Force Server Config");
+            _ = ConfigSync.AddLockingConfigEntry(_serverConfigLocked);
+        }
 
         public void Awake()
         {
-            _serverConfigLocked = config("General", "Force Server Config", true, "Force Server Config");
-            _ = ConfigSync.AddLockingConfigEntry(_serverConfigLocked);
+            Plugin = this;
+            InitConfigs();
 
-
-
-            #region 
-
-
-
-            #region Location Notes
-
-            // MapIcon                      Sets the map icon for the location.
-            // ShowMapIcon                  When to show the map icon of the location. Requires an icon to be set. Use "Never" to not show a map icon for the location. Use "Always" to always show a map icon for the location. Use "Explored" to start showing a map icon for the location as soon as a player has explored the area.
-            // MapIconSprite                Sets the map icon for the location.
-            // CanSpawn                     Can the location spawn at all.
-            // SpawnArea                    If the location should spawn more towards the edge of the biome or towards the center. Use "Edge" to make it spawn towards the edge. Use "Median" to make it spawn towards the center. Use "Everything" if it doesn't matter.</para>
-            // Prioritize                   If set to true, this location will be prioritized over other locations, if they would spawn in the same area.
-            // PreferCenter                 If set to true, Valheim will try to spawn your location as close to the center of the map as possible.
-            // Rotation                     How to rotate the location. Use "Fixed" to use the rotation of the prefab. Use "Random" to randomize the rotation. Use "Slope" to rotate the location along a possible slope.
-            // HeightDelta                  The minimum and maximum height difference of the terrain below the location.
-            // SnapToWater                  If the location should spawn near water.
-            // ForestThreshold              If the location should spawn in a forest. Everything above 1.15 is considered a forest by Valheim. 2.19 is considered a thick forest by Valheim.
-            // Biome
-            // SpawnDistance                Minimum and maximum range from the center of the map for the location.
-            // SpawnAltitude                Minimum and maximum altitude for the location.
-            // MinimumDistanceFromGroup     Locations in the same group will keep at least this much distance between each other.
-            // GroupName                    The name of the group of the location, used by the minimum distance from group setting.
-            // Count                        Maximum number of locations to spawn in. Does not mean that this many locations will spawn. But Valheim will try its best to spawn this many, if there is space.
-            // Unique                       If set to true, all other locations will be deleted, once the first one has been discovered by a player.
-
-            #endregion
-
-            #region Pets
-
+            // Example
+            // Use the Pet Manager, to load both Creature and Item within the same class
+            // Define your creature using [variable].creature...
+            // Define your item using [variable].item...
+            // Setup the SE base values of equipping item using: [variable].EquipStatusEffect.config...
             
+            PetManager.Pet MountLoxYgg0 = new PetManager.Pet("pets", "Mount_Lox_Ygg0", "Mount_Lox_Ygg0_Item");
+            MountLoxYgg0.creature.CanBeTamed = true;
+            MountLoxYgg0.creature.FoodItems = "Raspberry";
+            MountLoxYgg0.creature.CanSpawn = false;
+            MountLoxYgg0.item.Name.English("Lox Token");
+            MountLoxYgg0.item.Description.English("Spawns a loyal lox that follows you around");
+            MountLoxYgg0.item.Crafting.Add(CraftingTable.Workbench, 1);
+            MountLoxYgg0.item.RequiredItems.Add("Coins", 10);
+            MountLoxYgg0.EquipStatusEffect.config.CarryWeight.Value = 100f;
+            MountLoxYgg0.SpawnEffects.Add("vfx_spawn");
+            MountLoxYgg0.SpawnEffects.Add("sfx_spawn");
+            
+            // LoadCreatures(); // Any creatures that are not pets can be loaded using classic Creature Manager
+            LoadItems();
+            RegisterEffects();
+            
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            _harmony.PatchAll(assembly);
+            SetupWatcher();
+        }
 
+        private static void LoadCreatures()
+        {
             Creature Mount_Lox_Ygg0 = new("pets", "Mount_Lox_Ygg0")
             {
                 CanBeTamed = true,
@@ -163,16 +159,53 @@ namespace pets
                 FoodItems = ("Raspberry"),
                 CanSpawn = false,
             };
+        }
+        private static void RegisterEffects()
+        {
+            //FX, VFX and SFX//
+
+            GameObject vfx_petblood_Ygg = ItemManager.PrefabManager.RegisterPrefab("pets", "vfx_petblood_Ygg");
+            GameObject fx_startsummon_petmount_Ygg = ItemManager.PrefabManager.RegisterPrefab("pets", "fx_startsummon_petmount_Ygg");
+            GameObject fx_summonend_petmount_ygg = ItemManager.PrefabManager.RegisterPrefab("pets", "fx_summonend_petmount_ygg");
+            GameObject Orb_ProjectilePetMount_Ygg = ItemManager.PrefabManager.RegisterPrefab("pets", "Orb_ProjectilePetMount_Ygg");
+
+            GameObject Projectile_Pet_Ygg = ItemManager.PrefabManager.RegisterPrefab("pets", "Projectile_Pet_Ygg");
+
+            GameObject sfx_Duck_alerted_Ygg = ItemManager.PrefabManager.RegisterPrefab("pets", "sfx_Duck_alerted_Ygg");
+            GameObject sfx_duck_love_Ygg = ItemManager.PrefabManager.RegisterPrefab("pets", "sfx_duck_love_Ygg");
+            GameObject vfx_love_petmount_ygg = ItemManager.PrefabManager.RegisterPrefab("pets", "vfx_love_petmount_ygg");
+        }
+        private static void LoadItems()
+        {
+            LoadSaddles();
+            LoadCreatureAttackItems();
             
+            Item Pet_Orb_Boar1_Ygg = new("pets", "Pet_Orb_Boar1_Ygg");
+            Pet_Orb_Boar1_Ygg.Name.English("Boar Orb");
+            Pet_Orb_Boar1_Ygg.Description.English("Summon Boar");
+        }
+        private static void LoadCreatureAttackItems()
+        {
+            //Item Atk, Projectile or Sleep//
 
+            Item Boar_Chill_Ygg = new("pets", "Boar_Chill_Ygg");
+            Boar_Chill_Ygg.Name.English("Chill Boar");
+            Boar_Chill_Ygg.Description.English("Chilly Boar.");
 
+            Item Boar_Sleep_Ygg = new("pets", "Boar_Sleep_Ygg");
+            Boar_Sleep_Ygg.Name.English("Sleep Boar");
+            Boar_Sleep_Ygg.Description.English("Sleepy Boar.");
 
+            Item Boar_Tusk = new("pets", "Boar_Tusk");
+            Boar_Tusk.Name.English("Tusk Boar");
+            Boar_Tusk.Description.English("Tusk Boar.");
 
-
-
-
-            //Items//
-
+            Item Duck_Sleep_Ygg = new("pets", "Duck_Sleep_Ygg");
+            Duck_Sleep_Ygg.Name.English("Duck Chills");
+            Duck_Sleep_Ygg.Description.English("Duck Chills.");
+        }
+        private static void LoadSaddles()
+        {
             Item Saddle_Pet_Ygg1 = new("pets", "Saddle_Pet_Ygg1");
             Saddle_Pet_Ygg1.Name.English("Hide Pet Saddle");
             Saddle_Pet_Ygg1.Description.English("Saddle for a Boar pet.");
@@ -208,74 +241,8 @@ namespace pets
             Saddle_Pet_Lox2.RequiredItems.Add("Resin", 2);
             Saddle_Pet_Lox2.RequiredItems.Add("Honey", 1);
             Saddle_Pet_Lox2.CraftAmount = 1;
-            
-
-
-
-
-            Item Pet_Orb_Boar1_Ygg = new("pets", "Pet_Orb_Boar1_Ygg");
-            Pet_Orb_Boar1_Ygg.Name.English("Boar Orb");
-            Pet_Orb_Boar1_Ygg.Description.English("Summon Boar");
-
-
-
-
-            //Item Atk, Projectile or Sleep//
-
-            Item Boar_Chill_Ygg = new("pets", "Boar_Chill_Ygg");
-            Boar_Chill_Ygg.Name.English("Chill Boar");
-            Boar_Chill_Ygg.Description.English("Chilly Boar.");
-
-            Item Boar_Sleep_Ygg = new("pets", "Boar_Sleep_Ygg");
-            Boar_Sleep_Ygg.Name.English("Sleep Boar");
-            Boar_Sleep_Ygg.Description.English("Sleepy Boar.");
-
-            Item Boar_Tusk = new("pets", "Boar_Tusk");
-            Boar_Tusk.Name.English("Tusk Boar");
-            Boar_Tusk.Description.English("Tusk Boar.");
-
-            Item Duck_Sleep_Ygg = new("pets", "Duck_Sleep_Ygg");
-            Duck_Sleep_Ygg.Name.English("Duck Chills");
-            Duck_Sleep_Ygg.Description.English("Duck Chills.");
-
-            
-
-
-
-
-
-            //FX, VFX and SFX//
-
-            GameObject vfx_petblood_Ygg = ItemManager.PrefabManager.RegisterPrefab("pets", "vfx_petblood_Ygg");
-            GameObject fx_startsummon_petmount_Ygg = ItemManager.PrefabManager.RegisterPrefab("pets", "fx_startsummon_petmount_Ygg");
-            GameObject fx_summonend_petmount_ygg = ItemManager.PrefabManager.RegisterPrefab("pets", "fx_summonend_petmount_ygg");
-            GameObject Orb_ProjectilePetMount_Ygg = ItemManager.PrefabManager.RegisterPrefab("pets", "Orb_ProjectilePetMount_Ygg");
-
-            GameObject Projectile_Pet_Ygg = ItemManager.PrefabManager.RegisterPrefab("pets", "Projectile_Pet_Ygg");
-
-            GameObject sfx_Duck_alerted_Ygg = ItemManager.PrefabManager.RegisterPrefab("pets", "sfx_Duck_alerted_Ygg");
-            GameObject sfx_duck_love_Ygg = ItemManager.PrefabManager.RegisterPrefab("pets", "sfx_duck_love_Ygg");
-            GameObject vfx_love_petmount_ygg = ItemManager.PrefabManager.RegisterPrefab("pets", "vfx_love_petmount_ygg");
-
-            #endregion
-
-
-
-
-
-            #endregion
-
-
-            Assembly assembly = Assembly.GetExecutingAssembly();
-            _harmony.PatchAll(assembly);
-            SetupWatcher();
         }
-
-        private void OnDestroy()
-        {
-            Config.Save();
-        }
-
+        private void OnDestroy() => Config.Save();
         private void SetupWatcher()
         {
             FileSystemWatcher watcher = new(Paths.ConfigPath, ConfigFileName);
@@ -286,7 +253,6 @@ namespace pets
             watcher.SynchronizingObject = ThreadingHelper.SynchronizingObject;
             watcher.EnableRaisingEvents = true;
         }
-
         private void ReadConfigValues(object sender, FileSystemEventArgs e)
         {
             if (!File.Exists(ConfigFileFullPath)) return;
@@ -301,14 +267,7 @@ namespace pets
                 petsLogger.LogError("Please check your config entries for spelling and format!");
             }
         }
-
-
-        #region ConfigOptions
-
-        private static ConfigEntry<bool>? _serverConfigLocked;
-
-        private ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description,
-            bool synchronizedSetting = true)
+        public ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description, bool synchronizedSetting = true)
         {
             ConfigDescription extendedDescription =
                 new(
@@ -323,30 +282,30 @@ namespace pets
 
             return configEntry;
         }
-
-        private ConfigEntry<T> config<T>(string group, string name, T value, string description,
-            bool synchronizedSetting = true)
+        private ConfigEntry<T> config<T>(string group, string name, T value, string description, bool synchronizedSetting = true)
         {
             return config(group, name, value, new ConfigDescription(description), synchronizedSetting);
         }
-
-        private class ConfigurationManagerAttributes
+        public class ConfigurationManagerAttributes
         {
-            public bool? Browsable = false;
+            [UsedImplicitly] public int? Order;
+            [UsedImplicitly] public bool? Browsable;
+            [UsedImplicitly] public string? Category;
+            [UsedImplicitly] public Action<ConfigEntryBase>? CustomDrawer;
         }
-        class AcceptableShortcuts : AcceptableValueBase
+    }
+    public static class KeyboardExtensions
+    {
+        public static bool IsKeyDown(this KeyboardShortcut shortcut)
         {
-            public AcceptableShortcuts() : base(typeof(KeyboardShortcut))
-            {
-            }
-
-            public override object Clamp(object value) => value;
-            public override bool IsValid(object value) => true;
-
-            public override string ToDescriptionString() =>
-                "# Acceptable values: " + string.Join(", ", KeyboardShortcut.AllKeyCodes);
+            return shortcut.MainKey != KeyCode.None && Input.GetKeyDown(shortcut.MainKey) &&
+                   shortcut.Modifiers.All(Input.GetKey);
         }
 
-        #endregion
+        public static bool IsKeyHeld(this KeyboardShortcut shortcut)
+        {
+            return shortcut.MainKey != KeyCode.None && Input.GetKey(shortcut.MainKey) &&
+                   shortcut.Modifiers.All(Input.GetKey);
+        }
     }
 }
